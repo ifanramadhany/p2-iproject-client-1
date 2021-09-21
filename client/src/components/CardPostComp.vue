fb2a606058f03a4285f57b925580ddec2fcd3d04
ifanramadhany <ifanpersie14@gmail.com>
2021-09-21 19:03:02 +0700

    add homepage and homecard component

---
+++ client/src/components/CardPostComp.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="card-post">
+        <div class="main-post">
+          <div class="album">
+            <iframe title="deezer-widget" src="https://widget.deezer.com/widget/dark/track/1425844092" width="410" height="230" frameborder="0" allowtransparency="true" allow="encrypted-media; clipboard-write"></iframe>
+          </div>
+          <div class="title">STAY</div>
+          <div class="artist">The Kid Laroy</div>
+          <div class="embed">
+            <div style="margin-left: 6px;">
+              <img
+              src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+              alt=""
+              width="25px"
+              style="border-radius: 5px"
+            />
+            <p>amad1</p>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil temporibus ut iure enim doloremque amet?</p>
+          </div>
+        </div>
+
+        <div class="like-comment">
+          <a href="">
+            <img
+              style="margin-right: 5px"
+              src="https://img.icons8.com/ios-filled/40/000000/filled-like.png"
+            />
+            <span>125</span>
+          </a>
+          <a href="">
+            <img
+              src="https://img.icons8.com/material-outlined/40/000000/comments--v1.png"
+            />
+          </a>
+        </div>
+      </div>
+</template>
+
+<script>
+export default {
+  name: 'CardPostComp'
+}
+</script>
+
+<style>
+
+</style>
